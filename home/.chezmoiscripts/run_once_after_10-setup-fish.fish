3f7847c286b553d5f089199d3a7d7a0653430834
yuchenfei <cf.yu@qq.com>
2024-06-10 00:44:07 +0800

    Add chezmoi scriptes for fish shell

---
+++ home/.chezmoiscripts/run_once_after_10-setup-fish.fish
@@ -0,0 +1,8 @@
+#!/usr/bin/fish
+
+if not type -q fisher
+  echo "Installing fisher"
+  curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+end
+
+fisher update
