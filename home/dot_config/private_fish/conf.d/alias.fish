a5420175fd4cd920205e34822835b264a3f4c3b2
yuchenfei <cf.yu@qq.com>
2024-10-21 15:29:44 +0800

    fix: move brew config to env

--- home/dot_config/private_fish/conf.d/alias.fish
+++ home/dot_config/private_fish/conf.d/alias.fish
@@ -1,3 +1,27 @@
+abbr reload 'source ~/.config/fish/config.fish'
+
+abbr ff fastfetch
+abbr lg lazygit
+
+if type -q chezmoi
+  abbr c 'chezmoi'
+  abbr ca 'chezmoi apply'
+  abbr cu 'chezmoi update'
+  abbr ce 'chezmoi edit'
+  abbr cs 'chezmoi status'
+  abbr cc 'chezmoi cd'
+end
+
+if type -q docker-compose
+  abbr dc 'docker-compose'
+  abbr dcu 'docker-compose up'
+  abbr dcud 'docker-compose up -d'
+  abbr dcd 'docker-compose down'
+  abbr dcl 'docker-compose logs'
+  abbr dclf 'docker-compose logs -f'
+  abbr dcp 'docker-compose pull'
+end
+
 if type -q eza
   alias ls="eza --icons --no-quotes --group-directories-first"
   alias lsa="ls -a"
--- home/dot_config/private_fish/conf.d/path.fish
+++
@@ -1,3 +0,0 @@
-fish_add_path ~/.cargo/bin
-fish_add_path ~/miniconda3/bin
-fish_add_path ~/.rye/shims
