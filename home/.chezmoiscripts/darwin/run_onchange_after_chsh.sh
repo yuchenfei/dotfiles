39406d8baab8b47d8c0b5e43a315f761e2c8e014
yuchenfei <cf.yu@qq.com>
2024-10-24 15:27:02 +0800

    feat: add linux support

--- home/.chezmoiscripts/darwin/run_onchange_after_chsh.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-if [ $SHELL != $(which fish) ]; then
-  echo -e "\033[0;32m>>>>> Changing default shell to fish <<<<<\033[0m"
-  chsh -s $(which fish)
-fi
--- home/.chezmoiscripts/darwin/run_onchange_after_setup-packages.fish
+++
@@ -1,9 +0,0 @@
-#!/opt/homebrew/bin/fish
-
-if type -q ya
-  set_color green
-  echo ">>>>> Setup Yazi <<<<<"
-  set_color normal
-
-  ya pack -i
-end
