407785df0574753e09dc0858e654031d84947d28
yuchenfei <cf.yu@qq.com>
2024-12-06 20:50:40 +0800

    feat(nvim): new nvim config from kickstart

---
+++ home/dot_config/nvim/lua/plugins/tool/which-key.lua
@@ -0,0 +1,18 @@
+-- Useful plugin to show you pending keybinds.
+-- Github: https://github.com/folke/which-key.nvim
+return {
+  'folke/which-key.nvim',
+  event = 'VeryLazy',
+  opts = {
+    -- Document existing key chains
+    spec = {
+      { '<leader>c', group = '[C]ode', mode = { 'n', 'x' } },
+      { '<leader>d', group = '[D]ocument' },
+      { '<leader>r', group = '[R]ename' },
+      { '<leader>s', group = '[S]earch' },
+      { '<leader>w', group = '[W]orkspace' },
+      { '<leader>t', group = '[T]oggle' },
+      { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
+    },
+  },
+}
