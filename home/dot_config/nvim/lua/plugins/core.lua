ef5acd3101513f1c73bb24409552f739acd47054
yuchenfei <cf.yu@qq.com>
2024-12-18 15:49:16 +0800

    feat(nvim): config colorscheme

---
+++ home/dot_config/nvim/lua/plugins/core.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+  { -- https://github.com/catppuccin/nvim
+    "catppuccin",
+    opts = {
+      flavour = "auto",
+      background = {
+        light = "latte",
+        dark = "mocha",
+      },
+      transparent_background = true,
+    },
+  },
+}
