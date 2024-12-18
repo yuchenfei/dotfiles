c2808ef6d80fa3271bd6f20df05139a1aa9017f4
yuchenfei <cf.yu@qq.com>
2024-12-18 15:00:12 +0800

    feat(nvim): lazyvim as default

--- home/dot_config/private_fish/conf.d/nvim.fish
+++ home/dot_config/private_fish/conf.d/nvim.fish
@@ -1,15 +1,16 @@
-alias nvim-lazy="NVIM_APPNAME=LazyVim nvim"
-alias nvim-astro="NVIM_APPNAME=AstroNvim nvim"
+alias vl="NVIM_APPNAME=nvim-lazyvim nvim"
+alias va="NVIM_APPNAME=nvim-astronvim nvim"
+alias vc="NVIM_APPNAME=nvim-custom nvim"
 
 # default nvim
-set -gx NVIM_APPNAME AstroNvim
+# set -gx NVIM_APPNAME AstroNvim
 
 # nvim
 if type -q nvim
-  abbr v 'nvim'
-  abbr vi 'nvim'
-  abbr vim 'nvim'
-  abbr sv 'sudo nvim'
-  abbr svi 'sudo nvim'
-  abbr svim 'sudo nvim'
+    abbr v nvim
+    abbr vi nvim
+    abbr vim nvim
+    abbr sv 'sudo nvim'
+    abbr svi 'sudo nvim'
+    abbr svim 'sudo nvim'
 end
