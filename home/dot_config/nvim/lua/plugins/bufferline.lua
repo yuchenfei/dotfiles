f61c3e54b848426e5ca5158c8544fb8379084934
yuchenfei <cf.yu@qq.com>
2024-12-26 14:28:50 +0800

    fix(nvim): bufferline add neo-tree separator

---
+++ home/dot_config/nvim/lua/plugins/bufferline.lua
@@ -0,0 +1,16 @@
+return {
+  "akinsho/bufferline.nvim",
+  opts = {
+    options = {
+      offsets = {
+        {
+          filetype = "neo-tree",
+          text = "Neo-tree",
+          highlight = "Directory",
+          text_align = "left",
+          separator = true,
+        },
+      },
+    },
+  },
+}
