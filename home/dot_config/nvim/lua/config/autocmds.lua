1a9f4f3f10fbc75ae18df8142b87cc793945a142
yuchenfei <cf.yu@qq.com>
2025-01-01 14:40:57 +0800

    feat(nvim): add eslint and auto format

--- home/dot_config/nvim/lua/config/autocmds.lua
+++ home/dot_config/nvim/lua/config/autocmds.lua
@@ -7,10 +7,21 @@
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 
+-- Toggle theme based on background
 vim.api.nvim_create_autocmd("OptionSet", {
   pattern = "background",
   callback = function()
     local background = vim.o.background
     os.execute("fish -c 'toggle_theme " .. background .. "'")
+  end,
+})
+
+-- Auto organize imports on save
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+  group = vim.api.nvim_create_augroup("ts_organize_imports", { clear = true }),
+  pattern = "*.tsx,*.ts",
+  callback = function()
+    vim.lsp.buf.code_action({ apply = true, context = { only = { "source.addMissingImports.ts" }, diagnostics = {} } })
+    vim.lsp.buf.code_action({ apply = true, context = { only = { "source.removeUnused.ts" }, diagnostics = {} } })
   end,
 })
--- home/dot_config/nvim/lua/config/options.lua
+++ home/dot_config/nvim/lua/config/options.lua
@@ -6,6 +6,9 @@
 -- If no prettier config file is found, the formatter will not be used
 vim.g.lazyvim_prettier_needs_config = false
 
+-- Set to false to disable auto format
+vim.g.lazyvim_eslint_auto_format = true
+
 vim.opt.background = "dark"
 
 vim.opt.wrap = true
