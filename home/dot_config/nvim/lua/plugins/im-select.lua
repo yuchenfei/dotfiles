63eb0748c0ef8b6d889a4d0a304fbc5676062d91
yuchenfei <cf.yu@qq.com>
2025-01-04 16:09:16 +0800

    feat(nvim): add im-select for auto switch input source

---
+++ home/dot_config/nvim/lua/plugins/im-select.lua
@@ -0,0 +1,8 @@
+-- https://github.com/keaising/im-select.nvim
+return {
+  "keaising/im-select.nvim",
+  opts = {
+    default_im_select = "com.apple.keylayout.ABC",
+    default_command = "im-select",
+  },
+}
