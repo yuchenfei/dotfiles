fffd609709311d817648f588801e1a139baf3cf3
yuchenfei <cf.yu@qq.com>
2025-01-04 14:54:31 +0800

    fix(nvim): update obsidian config

--- home/dot_config/nvim/lua/plugins/obsidian.lua
+++ home/dot_config/nvim/lua/plugins/obsidian.lua
@@ -18,41 +18,14 @@ return {
           path = "~/Documents/Obsidian/Notes/",
         },
       },
-      completion = {
-        nvim_cmp = false,
-      },
     },
-    config = function(_, opts)
-      require("obsidian").setup(opts)
-
-      -- HACK: fix error: disable completion.nvim_cmp option, manually register sources
-      -- https://github.com/epwalsh/obsidian.nvim/issues/770#issuecomment-2516398527
-      local cmp = require("cmp")
-      cmp.register_source("obsidian", require("cmp_obsidian").new())
-      cmp.register_source("obsidian_new", require("cmp_obsidian_new").new())
-      cmp.register_source("obsidian_tags", require("cmp_obsidian_tags").new())
-    end,
   },
   {
     "saghen/blink.cmp",
     dependencies = { "saghen/blink.compat" },
     opts = {
       sources = {
-        default = { "obsidian", "obsidian_new", "obsidian_tags" },
-        providers = {
-          obsidian = {
-            name = "obsidian",
-            module = "blink.compat.source",
-          },
-          obsidian_new = {
-            name = "obsidian_new",
-            module = "blink.compat.source",
-          },
-          obsidian_tags = {
-            name = "obsidian_tags",
-            module = "blink.compat.source",
-          },
-        },
+        compat = { "obsidian", "obsidian_new", "obsidian_tags" },
       },
     },
   },
