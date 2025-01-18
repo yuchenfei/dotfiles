5a8c90c64175fe71632d478a13beac89c32a462f
yuchenfei <cf.yu@qq.com>
2025-01-18 16:54:42 +0800

    feat(nvim): custom copilot chat config

--- home/dot_config/nvim/lua/plugins/copilot.lua
+++ home/dot_config/nvim/lua/plugins/copilot.lua
@@ -1,13 +1,26 @@
 return {
-  "zbirenbaum/copilot.lua",
-  opts = {
-    suggestion = {
-      keymap = {
-        accept_word = "<C-w>",
-        accept_line = "<C-l>",
-        next = "<C-x>",
-        prev = "<C-z>",
-        dismiss = "<C-c>",
+  {
+    "zbirenbaum/copilot.lua",
+    opts = {
+      suggestion = {
+        keymap = {
+          accept_word = "<C-w>",
+          accept_line = "<C-l>",
+          next = "<C-x>",
+          prev = "<C-z>",
+          dismiss = "<C-c>",
+        },
+      },
+    },
+  },
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    opts = {
+      -- debug = true,
+      -- log_level = "trace",
+      prompts = {
+        Spelling = { prompt = "Please correct any grammar and spelling errors in the following text." },
+        Translation = { prompt = "请将文本翻译成中文" },
       },
     },
   },
