4105ac950f3ec72b371398e80c79f3fe64b5c68c
yuchenfei <cf.yu@qq.com>
2025-01-26 00:11:32 +0800

    fix(nvim): better-escape default config interferes with lazygit

--- home/dot_config/nvim/lua/plugins/better-escape.lua
+++ home/dot_config/nvim/lua/plugins/better-escape.lua
@@ -1,5 +1,9 @@
 -- https://github.com/max397574/better-escape.nvim
 return {
   "max397574/better-escape.nvim",
-  opts = true,
+  opts = {
+    mappings = {
+      t = { j = { false } },
+    },
+  },
 }
