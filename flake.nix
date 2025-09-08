20d1a7bb66739c49955812cc0df1492265f9d747
yuchenfei <cf.yu@qq.com>
2025-09-09 03:46:11 +0800

    init nix-darwin

---
+++ flake.nix
@@ -0,0 +1,149 @@
+{
+  description = "Chenfei nix-darwin system flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:nix-darwin/nix-darwin/master";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    nix-homebrew.url = "github:zhaofengli/nix-homebrew";
+
+    # Optional: Declarative tap management
+    homebrew-core = {
+      url = "github:homebrew/homebrew-core";
+      flake = false;
+    };
+    homebrew-cask = {
+      url = "github:homebrew/homebrew-cask";
+      flake = false;
+    };
+  };
+
+  outputs =
+    inputs@{
+      self,
+      nix-darwin,
+      nixpkgs,
+      home-manager,
+      nix-homebrew,
+      homebrew-core,
+      homebrew-cask,
+    }:
+    let
+      configuration =
+        { pkgs, config, ... }:
+        {
+          # List packages installed in system profile. To search by name, run:
+          # $ nix-env -qaP | grep wget
+          environment.systemPackages = [
+            pkgs.mkalias
+          ];
+
+          programs.zsh.enable = true;
+          services.skhd.enable = true;
+          services.yabai.enable = true;
+
+          fonts.packages = with pkgs; [
+            nerd-fonts.jetbrains-mono
+          ];
+
+          homebrew = {
+            enable = true;
+            brews = [
+              "mas"
+            ];
+          };
+
+          users.users.yuchenfei.home = "/Users/yuchenfei";
+          system.primaryUser = "yuchenfei";
+
+          security.pam.services.sudo_local.touchIdAuth = true;
+          security.pam.services.sudo_local.watchIdAuth = true;
+
+          system.defaults = {
+            dock.autohide = true;
+          };
+
+          # https://gist.github.com/elliottminns/211ef645ebd484eb9a5228570bb60ec3
+          system.activationScripts.applications.text =
+            let
+              env = pkgs.buildEnv {
+                name = "system-applications";
+                paths = config.environment.systemPackages;
+                pathsToLink = "/Applications";
+              };
+            in
+            pkgs.lib.mkForce ''
+              # Set up applications.
+              echo "setting up /Applications..." >&2
+              rm -rf /Applications/Nix\ Apps
+              mkdir -p /Applications/Nix\ Apps
+              find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+              while read -r src; do
+                app_name=$(basename "$src")
+                echo "copying $src" >&2
+                ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+              done
+            '';
+
+          # Necessary for using flakes on this system.
+          nix.settings.experimental-features = "nix-command flakes";
+
+          # Set Git commit hash for darwin-version.
+          system.configurationRevision = self.rev or self.dirtyRev or null;
+
+          # Used for backwards compatibility, please read the changelog before changing.
+          # $ darwin-rebuild changelog
+          system.stateVersion = 6;
+
+          # The platform the configuration will be used on.
+          nixpkgs.hostPlatform = "aarch64-darwin";
+        };
+    in
+    {
+      # Build darwin flake using:
+      # $ darwin-rebuild build --flake .#MAC-YCF
+      darwinConfigurations."MAC-YCF" = nix-darwin.lib.darwinSystem {
+        modules = [
+          configuration
+          home-manager.darwinModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.yuchenfei = ./home.nix;
+
+            # Optionally, use home-manager.extraSpecialArgs to pass
+            # arguments to home.nix
+          }
+          nix-homebrew.darwinModules.nix-homebrew
+          {
+            nix-homebrew = {
+              # Install Homebrew under the default prefix
+              enable = true;
+
+              # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
+              enableRosetta = true;
+
+              # User owning the Homebrew prefix
+              user = "yuchenfei";
+
+              # Automatically migrate existing Homebrew installations
+              autoMigrate = true;
+
+              # Optional: Declarative tap management
+              taps = {
+                "homebrew/homebrew-core" = homebrew-core;
+                "homebrew/homebrew-cask" = homebrew-cask;
+              };
+
+              # Optional: Enable fully-declarative tap management
+              #
+              # With mutableTaps disabled, taps can no longer be added imperatively with `brew tap`.
+              mutableTaps = false;
+            };
+          }
+        ];
+      };
+    };
+}
---
+++ home.nix
@@ -0,0 +1,40 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "yuchenfei";
+  home.homeDirectory = "/Users/yuchenfei";
+
+  # Packages that should be installed to the user profile.
+  home.packages = [
+    pkgs.neovim
+    pkgs.nixfmt-rfc-style
+  ];
+
+  programs.git = {
+    enable = true;
+    delta.enable = true;
+    userName = "yuchenfei";
+    userEmail = "cf.yu@qq.com";
+    extraConfig = {
+      init.defaultBranch = "main";
+      merge.conflictStyle = "zdiff3";
+      delta.navigate = true;
+      delta.line-numbers = true;
+    };
+  };
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "25.05";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
