0ad1679a3f123a26994504e0b85b4185a2ac8b1d
yuchenfei <cf.yu@qq.com>
2025-09-16 21:12:21 +0800

    modify yabai config

--- darwin.nix
+++ darwin.nix
@@ -54,16 +54,6 @@
     pkgs.mkalias
   ];
 
-  fonts.packages = with pkgs; [
-    nerd-fonts.jetbrains-mono
-    maple-mono.NF-CN-unhinted
-  ];
-
-  programs.zsh.enable = true;
-  programs.fish.enable = true;
-  services.skhd.enable = true;
-  services.yabai.enable = true;
-
   homebrew = {
     enable = true;
     brews = [
@@ -71,8 +61,46 @@
     ];
   };
 
+  fonts.packages = with pkgs; [
+    nerd-fonts.jetbrains-mono
+    maple-mono.NF-CN-unhinted
+  ];
+
   users.users.${user} = {
     home = "/Users/${user}";
+  };
+
+  programs.zsh.enable = true;
+  programs.fish.enable = true;
+
+  services.skhd.enable = true;
+  services.yabai = {
+    enable = true;
+    enableScriptingAddition = true; # SIP must be disabled
+    config = {
+      # https://github.com/koekeishiya/yabai/wiki/Configuration#configuration-file
+      layout = "bsp";
+      window_placement = "second_child";
+      top_padding = 10;
+      bottom_padding = 10;
+      left_padding = 10;
+      right_padding = 10;
+      window_gap = 5;
+      window_shadow = "on";
+      window_opacity = "off";
+      active_window_opacity = "1.0";
+      normal_window_opacity = "0.9";
+    };
+    # grid=<rows>:<cols>:<start-x>:<start-y>:<width>:<height>
+    extraConfig = ''
+      yabai -m rule --add app="(System Preferences|系统设置)" manage=off grid=4:6:2:0:2:3
+      yabai -m rule --add app="(Finder|访达)" manage=off grid=20:20:1:1:18:18
+      yabai -m rule --add app="(Preview|预览)" manage=off
+      yabai -m rule --add app="(Music|音乐)" manage=off grid=10:10:1:1:8:8
+      yabai -m rule --add app="(Eudic|欧路词典)" manage=off grid=10:10:1:1:8:8
+      yabai -m rule --add app="微信" manage=off grid=8:5:1:1:3:6
+      yabai -m rule --add app="微信" title="^(视频通话.*)$" manage=off
+    '';
   };
 
   system = {
