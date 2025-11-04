2c503179fba758972515db6922f3f13ae87c8e15
yuchenfei <cf.yu@qq.com>
2025-11-04 14:05:19 +0800

    feat(nvim): add flash.nvim

---
+++ .config/nvim/lua/plugins/flash.lua
@@ -0,0 +1,59 @@
+-- Flash enhances the built-in search functionality by showing labels
+-- at the end of each match, letting you quickly jump to a specific
+-- location.
+--
+-- References:
+--  - https://github.com/folke/flash.nvim
+--  - https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/plugins/editor.lua
+
+return {
+  'folke/flash.nvim',
+  event = 'VeryLazy',
+  ---@type Flash.Config
+  opts = {
+    jump = {
+      autojump = true,
+    },
+    label = {
+      rainbow = {
+        enabled = true,
+      },
+    },
+  },
+  keys = {
+    { 's', mode = { 'n', 'x', 'o' }, function() require('flash').jump() end, desc = 'Flash' },
+    {
+      'S',
+      mode = { 'n', 'x', 'o' },
+      function() require('flash').treesitter() end,
+      desc = 'Flash Treesitter',
+    },
+    { 'r', mode = 'o', function() require('flash').remote() end, desc = 'Remote Flash' },
+    {
+      'R',
+      mode = { 'o', 'x' },
+      function() require('flash').treesitter_search() end,
+      desc = 'Treesitter Search',
+    },
+    {
+      '<c-s>',
+      mode = { 'c' },
+      function() require('flash').toggle() end,
+      desc = 'Toggle Flash Search',
+    },
+    -- Simulate nvim-treesitter incremental selection
+    {
+      '<c-a>',
+      mode = { 'n', 'o', 'x' },
+      function()
+        require('flash').treesitter({
+          actions = {
+            ['<c-a>'] = 'next',
+            ['<c-s>'] = 'prev',
+          },
+        })
+      end,
+      desc = 'Treesitter Incremental Selection',
+    },
+  },
+}
