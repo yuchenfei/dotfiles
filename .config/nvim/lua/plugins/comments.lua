da727b218322f82e589171354df651386861aa79
yuchenfei <cf.yu@qq.com>
2025-11-04 14:28:36 +0800

    feat(nvim): add ts-comments.nvim

---
+++ .config/nvim/lua/plugins/comments.lua
@@ -0,0 +1,17 @@
+-- Improves comment syntax, lets Neovim handle multiple
+-- types of comments for a single language, and relaxes rules
+-- for uncommenting.
+--
+-- References:
+--  - https://github.com/folke/ts-comments.nvim
+--  - https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/plugins/coding.lua
+--
+-- Similar plugins:
+--  - https://github.com/nvim-mini/mini.nvim/blob/main/readmes/mini-comment.md
+--  - https://github.com/numToStr/Comment.nvim
+
+return {
+  'folke/ts-comments.nvim',
+  event = 'VeryLazy',
+  opts = {},
+}
