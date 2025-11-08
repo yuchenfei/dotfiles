11e375c2d4ba3c2453afa8546e2e32c07efa1f7c
yuchenfei <cf.yu@qq.com>
2025-11-08 17:20:20 +0800

    fix(nvim): add nvim-treesitter-textobjects for mini.ai and text objects move

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,8 @@
--- https://github.com/nvim-treesitter/nvim-treesitter/blob/main/README.md
--- https://github.com/nvim-treesitter/nvim-treesitter-context
--- https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+-- References:
+--  - https://github.com/nvim-treesitter/nvim-treesitter/blob/main/README.md
+--  - https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+--  - https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/plugins/treesitter.lua
+--  - https://github.com/nvim-treesitter/nvim-treesitter-context
 
 return {
   {
@@ -11,6 +13,49 @@ return {
     config = function() require('config.treesitter') end,
   },
   {
+    -- Support @function.outer, @class.outer, etc. textobjects
+    'nvim-treesitter/nvim-treesitter-textobjects',
+    branch = 'main',
+    event = 'VeryLazy',
+    opts = {
+      move = {
+        set_jumps = true,
+      },
+    },
+    keys = function()
+      -- stylua: ignore
+      local key_opts = {
+        goto_next_start     = {[']f'] = '@function.outer', [']c'] = '@class.outer', [']a'] = '@parameter.inner',},
+        goto_next_end       = {[']F'] = '@function.outer', [']C'] = '@class.outer', [']A'] = '@parameter.inner',},
+        goto_previous_start = {['[f'] = '@function.outer', ['[c'] = '@class.outer', ['[a'] = '@parameter.inner',},
+        goto_previous_end   = {['[F'] = '@function.outer', ['[C'] = '@class.outer', ['[A'] = '@parameter.inner',},
+      }
+
+      local keys = {}
+      for method, keymaps in pairs(key_opts) do
+        for key, query in pairs(keymaps) do
+          local queries = type(query) == 'table' and query or { query }
+          local parts = {}
+          for _, q in ipairs(queries) do
+            local part = q:gsub('@', ''):gsub('%..*', '')
+            part = part:sub(1, 1):upper() .. part:sub(2)
+            table.insert(parts, part)
+          end
+          local desc = table.concat(parts, ' or ')
+          desc = 'TS: ' .. (key:sub(1, 1) == '[' and 'Prev ' or 'Next ') .. desc
+          desc = desc .. (key:sub(2, 2) == key:sub(2, 2):upper() and ' End' or ' Start')
+          table.insert(keys, {
+            key,
+            function() require('nvim-treesitter-textobjects.move')[method](query, 'textobjects') end,
+            desc = desc,
+          })
+        end
+      end
+      return keys
+    end,
+  },
+  {
+    -- Show code context (e.g., function/class name) at the top of the window while scrolling
     'nvim-treesitter/nvim-treesitter-context',
     event = { 'BufReadPost', 'BufNewFile' },
     opts = function()
@@ -34,9 +79,9 @@ return {
     end,
     keys = {
       {
-        '[c',
+        '[e',
         function() require('treesitter-context').go_to_context(vim.v.count1) end,
-        desc = 'Previous Context',
+        desc = 'TS: Previous Context',
       },
     },
   },
