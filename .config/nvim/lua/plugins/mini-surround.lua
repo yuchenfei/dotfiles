41450381c44949e6db7a0be9296fefcf7bd46df1
yuchenfei <cf.yu@qq.com>
2025-11-10 00:36:32 +0800

    chore(nvim): update mini.surround keymaps

--- .config/nvim/lua/plugins/mini-surround.lua
+++ .config/nvim/lua/plugins/mini-surround.lua
@@ -14,12 +14,12 @@ return {
   opts = {
     -- Module mappings. Use `''` (empty string) to disable one.
     mappings = {
-      add = 'sa', -- Add surrounding in Normal and Visual modes
-      delete = 'sd', -- Delete surrounding
-      find = 'sf', -- Find surrounding (to the right)
-      find_left = 'sF', -- Find surrounding (to the left)
-      highlight = 'sh', -- Highlight surrounding
-      replace = 'sr', -- Replace surrounding
+      add = 'gsa', -- Add surrounding in Normal and Visual modes
+      delete = 'gsd', -- Delete surrounding
+      find = 'gsf', -- Find surrounding (to the right)
+      find_left = 'gsF', -- Find surrounding (to the left)
+      highlight = 'gsh', -- Highlight surrounding
+      replace = 'gsr', -- Replace surrounding
 
       suffix_last = 'l', -- Suffix to search with "prev" method
       suffix_next = 'n', -- Suffix to search with "next" method
