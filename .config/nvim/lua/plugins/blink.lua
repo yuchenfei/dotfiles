05e66ff3fd68459dce4fd71c2ecde07c7f7ab1cb
yuchenfei <cf.yu@qq.com>
2025-11-11 12:17:17 +0800

    chore(nvim): config blink.cmp

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -1,11 +1,17 @@
--- https://cmp.saghen.dev/installation
--- https://github.com/rafamadriz/friendly-snippets
+-- References:
+-- - https://cmp.saghen.dev/installation
+-- - https://github.com/rafamadriz/friendly-snippets
+-- - https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/plugins/extras/coding/blink.lua
 
 return {
   'saghen/blink.cmp',
-  event = 'VimEnter',
   version = '1.*',
-  dependencies = { 'rafamadriz/friendly-snippets', 'folke/lazydev.nvim' },
+  dependencies = {
+    'rafamadriz/friendly-snippets',
+    'fang2hou/blink-copilot',
+    'folke/lazydev.nvim',
+  },
+  event = { 'InsertEnter', 'CmdlineEnter' },
   ---@module 'blink.cmp'
   ---@type blink.cmp.Config
   opts = {
@@ -25,17 +31,88 @@ return {
     -- <c-k>: Toggle signature help
     --
     -- See :h blink-cmp-config-keymap for defining your own keymap
-    keymap = { preset = 'default' },
-    sources = {
-      default = { 'lsp', 'path', 'snippets', 'lazydev', 'buffer' },
-      providers = {
-        lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
-      },
+    keymap = {
+      preset = 'default',
+      ['<C-space>'] = false,
+      ['<M-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
+      ['<PageDown>'] = { 'scroll_documentation_down', 'fallback' },
+      ['<PageUp>'] = { 'scroll_documentation_up', 'fallback' },
+      ['<C-u>'] = { function(cmp) return cmp.select_prev({ count = 5 }) end, 'fallback' },
+      ['<C-d>'] = { function(cmp) return cmp.select_next({ count = 5 }) end, 'fallback' },
     },
     completion = {
-      documentation = { auto_show = true, auto_show_delay_ms = 500 },
+      menu = {
+        draw = {
+          columns = {
+            { 'label', 'label_description', gap = 1 },
+            { 'kind_icon', 'kind', gap = 1 },
+            -- { 'source_name' },
+          },
+          treesitter = { 'lsp' },
+        },
+        -- border = 'rounded', -- See `:h winborder`
+      },
+      documentation = {
+        auto_show = true,
+        auto_show_delay_ms = 200,
+        -- window = { border = 'rounded' },
+      },
+      ghost_text = {
+        enabled = true,
+      },
     },
-    -- Shows a signature help window while you type arguments for a function
-    signature = { enabled = true },
+    signature = {
+      enabled = true,
+      -- window = { border = 'rounded' },
+    },
+    cmdline = {
+      keymap = {
+        ['<C-space>'] = false,
+        ['<M-space>'] = { 'show', 'fallback' },
+        ['<Right>'] = false,
+        ['<Left>'] = false,
+        ['<C-u>'] = { function(cmp) return cmp.select_prev({ count = 5 }) end, 'fallback' },
+        ['<C-d>'] = { function(cmp) return cmp.select_next({ count = 5 }) end, 'fallback' },
+      },
+      completion = {
+        menu = {
+          auto_show = function() return vim.fn.getcmdtype() == ':' end,
+        },
+      },
+    },
+    sources = {
+      default = { 'lsp', 'buffer', 'snippets', 'path', 'copilot' },
+      per_filetype = {
+        lua = { inherit_defaults = true, 'lazydev' },
+      },
+      providers = {
+        -- Buffer completion from all open buffers
+        -- - https://cmp.saghen.dev/recipes#buffer-completion-from-all-open-buffers
+        buffer = {
+          opts = {
+            -- get all buffers, even ones like neo-tree
+            -- get_bufnrs = vim.api.nvim_list_bufs
+            -- or (recommended) filter to only "normal" buffers
+            get_bufnrs = function()
+              return vim.tbl_filter(
+                function(bufnr) return vim.bo[bufnr].buftype == '' end,
+                vim.api.nvim_list_bufs()
+              )
+            end,
+          },
+        },
+        copilot = {
+          name = 'Copilot',
+          module = 'blink-copilot',
+          score_offset = 100,
+          async = true,
+        },
+        lazydev = {
+          name = 'LazyDev',
+          module = 'lazydev.integrations.blink',
+          score_offset = 100,
+        },
+      },
+    },
   },
 }
--- .config/nvim/lua/plugins/catppuccin.lua
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -10,6 +10,14 @@ return {
     float = {
       transparent = true,
     },
+    custom_highlights = function(colors)
+      return {
+        -- Blink
+        -- https://github.com/catppuccin/nvim/blob/main/lua/catppuccin/groups/integrations/blink_cmp.lua
+        BlinkCmpMenu = { bg = colors.surface0 },
+        BlinkCmpDoc = { bg = colors.surface0 },
+      }
+    end,
     auto_integrations = true,
   },
   init = function() vim.cmd.colorscheme('catppuccin') end,
