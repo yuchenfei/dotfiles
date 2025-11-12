6af7dcd6adb1ca0988c5bc24e2532ecb8b144394
yuchenfei <cf.yu@qq.com>
2025-11-13 01:25:47 +0800

    feat(nvim): add dictionary and thesaurus completion sources to blink.cmp

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -8,9 +8,10 @@ return {
   'saghen/blink.cmp',
   version = '1.*',
   dependencies = {
-    'rafamadriz/friendly-snippets',
+    'archie-judd/blink-cmp-words',
     'fang2hou/blink-copilot',
     'folke/lazydev.nvim',
+    'rafamadriz/friendly-snippets',
   },
   event = { 'InsertEnter', 'CmdlineEnter' },
   ---@module 'blink.cmp'
@@ -45,18 +46,23 @@ return {
       menu = {
         draw = {
           columns = {
+            { 'kind_icon' },
             { 'label', 'label_description', gap = 1 },
-            { 'kind_icon', 'kind', gap = 1 },
+            { 'kind' },
             -- { 'source_name' },
           },
           treesitter = { 'lsp' },
         },
         -- border = 'rounded', -- See `:h winborder`
+        scrollbar = false,
       },
       documentation = {
         auto_show = true,
         auto_show_delay_ms = 200,
-        -- window = { border = 'rounded' },
+        window = {
+          -- border = 'rounded',
+          scrollbar = false,
+        },
       },
       ghost_text = {
         enabled = true,
@@ -82,8 +88,10 @@ return {
       },
     },
     sources = {
-      default = { 'lsp', 'buffer', 'snippets', 'path', 'copilot', 'lazydev' },
+      default = { 'lsp', 'buffer', 'snippets', 'path', 'copilot' },
       per_filetype = {
+        text = { 'dictionary' },
+        markdown = { 'thesaurus' },
         lua = { inherit_defaults = true, 'lazydev' },
       },
       providers = {
@@ -102,6 +110,11 @@ return {
             end,
           },
         },
+        snippets = {
+          should_show_items = function(ctx) -- avoid triggering snippets after . " ' chars.
+            return ctx.trigger.initial_kind ~= 'trigger_character'
+          end,
+        },
         copilot = {
           name = 'Copilot',
           module = 'blink-copilot',
@@ -112,6 +125,48 @@ return {
           name = 'LazyDev',
           module = 'lazydev.integrations.blink',
           score_offset = 100,
+        },
+        -- Use the thesaurus source
+        thesaurus = {
+          name = 'blink-cmp-words',
+          module = 'blink-cmp-words.thesaurus',
+          -- All available options
+          opts = {
+            -- A score offset applied to returned items.
+            -- By default the highest score is 0 (item 1 has a score of -1, item 2 of -2 etc..).
+            score_offset = 0,
+
+            -- Default pointers define the lexical relations listed under each definition,
+            -- see Pointer Symbols below.
+            -- Default is as below ("antonyms", "similar to" and "also see").
+            definition_pointers = { '!', '&', '^' },
+
+            -- The pointers that are considered similar words when using the thesaurus,
+            -- see Pointer Symbols below.
+            -- Default is as below ("similar to", "also see" }
+            similarity_pointers = { '&', '^' },
+
+            -- The depth of similar words to recurse when collecting synonyms. 1 is similar words,
+            -- 2 is similar words of similar words, etc. Increasing this may slow results.
+            similarity_depth = 2,
+          },
+        },
+        -- Use the dictionary source
+        dictionary = {
+          name = 'blink-cmp-words',
+          module = 'blink-cmp-words.dictionary',
+          -- All available options
+          opts = {
+            -- The number of characters required to trigger completion.
+            -- Set this higher if completion is slow, 3 is default.
+            dictionary_search_threshold = 3,
+
+            -- See above
+            score_offset = 0,
+
+            -- See above
+            definition_pointers = { '!', '&', '^' },
+          },
         },
       },
     },
