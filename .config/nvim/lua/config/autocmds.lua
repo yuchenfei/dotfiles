3f67f94fe896ac1cc236068aead240d665f662e6
yuchenfei <cf.yu@qq.com>
2025-11-17 16:25:11 +0800

    feat(nvim): add autocmds for window resize and quick-close filetypes

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -2,14 +2,59 @@
 -- https://github.com/nvim-lua/kickstart.nvim/blob/master/init.lua
 -- https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 
+local function augroup(name) return vim.api.nvim_create_augroup(name, { clear = true }) end
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
--- Highlight when yanking (copying) text
---  Try it with `yap` in normal mode
---  See `:help vim.hl.on_yank()`
 vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  desc = 'Highlight when yank',
+  group = augroup('highlight_yank'),
+  --  See `:help vim.hl.on_yank()`
   callback = function() vim.hl.on_yank() end,
+})
+
+vim.api.nvim_create_autocmd('VimResized', {
+  desc = 'Resize splits if window got resized',
+  group = augroup('resize_splits'),
+  callback = function()
+    local current_tab = vim.fn.tabpagenr()
+    vim.cmd('tabdo wincmd =')
+    vim.cmd('tabnext ' .. current_tab)
+  end,
+})
+
+vim.api.nvim_create_autocmd('FileType', {
+  desc = 'Close certain filetypes with q',
+  group = augroup('close_with_q'),
+  pattern = {
+    'PlenaryTestPopup',
+    'checkhealth',
+    'dbout',
+    'gitsigns-blame',
+    'grug-far',
+    'help',
+    'lspinfo',
+    'neotest-output',
+    'neotest-output-panel',
+    'neotest-summary',
+    'notify',
+    'qf',
+    'spectre_panel',
+    'startuptime',
+    'tsplayground',
+  },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+    vim.schedule(function()
+      vim.keymap.set('n', 'q', function()
+        vim.cmd('close')
+        pcall(vim.api.nvim_buf_delete, event.buf, { force = true })
+      end, {
+        buffer = event.buf,
+        silent = true,
+        desc = 'Quit buffer',
+      })
+    end)
+  end,
 })
