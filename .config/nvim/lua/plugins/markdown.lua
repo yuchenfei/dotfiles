5f0a25e03322e1d02d79632fb3d4ef94c94f8371
yuchenfei <cf.yu@qq.com>
2025-11-26 16:18:14 +0800

    feat(nvim): enhance markdown editing with custom keymaps and toggle plugin

--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -22,6 +22,49 @@ return {
   {
     'bullets-vim/bullets.vim',
     ft = { 'markdown' },
-    init = function() vim.g.bullets_pad_right = 0 end,
+    init = function()
+      vim.g.bullets_set_mappings = 0
+      vim.g.bullets_custom_mappings = {
+        { 'imap', '<cr>', '<Plug>(bullets-newline)' },
+        { 'inoremap', '<C-cr>', '<cr>' },
+        { 'nmap', 'o', '<Plug>(bullets-newline)' },
+
+        { 'vmap', 'gN', '<Plug>(bullets-renumber)' },
+        { 'nmap', 'gN', '<Plug>(bullets-renumber)' },
+
+        { 'imap', '<C-t>', '<Plug>(bullets-demote)' },
+        { 'nmap', '>>', '<Plug>(bullets-demote)' },
+        { 'vmap', '>', '<Plug>(bullets-demote)' },
+        { 'imap', '<C-d>', '<Plug>(bullets-promote)' },
+        { 'nmap', '<<', '<Plug>(bullets-promote)' },
+        { 'vmap', '<', '<Plug>(bullets-promote)' },
+      }
+      vim.g.bullets_pad_right = 0
+      vim.g.bullets_outline_levels = { 'num' }
+    end,
+  },
+  -- https://github.com/roodolv/markdown-toggle.nvim
+  {
+    'roodolv/markdown-toggle.nvim',
+    ft = { 'markdown' },
+    opts = {
+      use_default_keymaps = false,
+      enable_autolist = false, -- use bullets.vim
+      keymaps = {
+        toggle = {
+          ['<Leader>mh'] = 'heading',
+          ['<Leader>mH'] = 'heading_toggle',
+          ['<Leader>ml'] = 'list',
+          ['<Leader>mo'] = 'olist',
+          ['<Leader>mq'] = 'quote',
+          ['<Leader>mx'] = 'checkbox',
+        },
+        switch = {
+          ['<Leader>mL'] = 'switch_cycle_list_table',
+          ['<Leader>mU'] = 'switch_unmarked_only',
+          ['<Leader>mX'] = 'switch_cycle_box_table',
+        },
+      },
+    },
   },
 }
