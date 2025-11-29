649b01625970e1ef80cf2cedf5997319e6673ece
yuchenfei <cf.yu@qq.com>
2025-11-29 20:22:56 +0800

    refactor(nvim): consolidate Mason configuration into plugin spec

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -9,14 +9,55 @@ return {
     cmd = 'Mason',
     keys = { { '<leader>lm', '<cmd>Mason<cr>', desc = 'Mason' } },
     opts = {},
+    config = function(_, opts)
+      require('mason').setup(opts)
+
+      local mr = require('mason-registry')
+      mr:on('package:install:success', function()
+        vim.defer_fn(function()
+          -- trigger FileType event to possibly load this newly installed LSP server
+          require('lazy.core.handler.event').trigger({
+            event = 'FileType',
+            buf = vim.api.nvim_get_current_buf(),
+          })
+        end, 100)
+      end)
+
+      -- https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim/issues/39#issue-2080773359
+      vim.api.nvim_command('MasonToolsInstall')
+    end,
   },
   {
     'WhoIsSethDaniel/mason-tool-installer.nvim',
     dependencies = {
       'mason-org/mason.nvim',
       -- can be installed for the option to use lspconfig names instead of Mason names.
-      'mason-org/mason-lspconfig.nvim',
+      { 'mason-org/mason-lspconfig.nvim', config = function() end },
     },
-    config = function() require('config.mason') end,
+    cmd = 'MasonToolsInstall',
+    opts = {
+      ensure_installed = {
+        'tree-sitter-cli',
+        'html',
+        'jsonls',
+        'prettier',
+        -- Lua
+        'lua_ls',
+        'stylua',
+        -- Nix
+        'nil_ls',
+        -- Markdown
+        'marksman',
+        'markdownlint-cli2', -- https://github.com/DavidAnson/markdownlint-cli2
+        -- Python
+        'basedpyright',
+        'pyrefly',
+        'ruff', -- https://docs.astral.sh/ruff/editors/setup/
+        -- TypeScript/JavaScript
+        'vtsls',
+        'biome',
+        'js-debug-adapter',
+      },
+    },
   },
 }
