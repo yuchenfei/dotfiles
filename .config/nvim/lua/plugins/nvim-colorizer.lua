8223fa0e34d6f82c83836d2fc4ca6a731f715f67
yuchenfei <cf.yu@qq.com>
2025-12-15 20:19:40 +0800

    feat(nvim): configure colorizer with filetype-specific themes and exclusions

--- .config/nvim/lua/plugins/nvim-colorizer.lua
+++ .config/nvim/lua/plugins/nvim-colorizer.lua
@@ -4,10 +4,17 @@ return {
   'catgoose/nvim-colorizer.lua',
   event = { 'BufReadPre', 'BufNewFile' },
   opts = {
+    filetypes = {
+      '*', -- Highlight all files, but customize some others.
+      '!markdown', -- Exclude vim from highlighting.
+      lua = { -- use different theme for lua filetype
+        names_custom = function() return require('catppuccin.palettes').get_palette('mocha') end,
+      },
+    },
     lazy_load = true,
     user_default_options = {
       names = true,
-      names_custom = function() return require('catppuccin.palettes').get_palette('mocha') end,
+      -- names_custom = function() return require('catppuccin.palettes').get_palette('mocha') end,
     },
   },
 }
